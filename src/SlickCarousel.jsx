878ad05a32b80213c5955a654afa84bc3766c7c6
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-18 13:44:45 +0700

    Add settings to Slider and change css

--- src/SlickCarousel.jsx
+++ src/SlickCarousel.jsx
@@ -1,16 +1,30 @@
 import React from "react";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import { list } from "./data";
 import Slider from "react-slick";
 import { FaQuoteRight } from "react-icons/fa";
 
 const SlickCarousel = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    // fade: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnHover: true,
+  };
   return (
-    <section className="slider-container">
-      <Slider>
+    <section className="slick-container">
+      <Slider {...settings}>
         {list.map((person) => {
           const { id, image, name, quote, title } = person;
           return (
-            <article key={id} className="slide">
+            <article key={id}>
               <img src={image} alt={name} className="person-img" />
               <h5 className="name">{name}</h5>
               <p className="title">{title}</p>
