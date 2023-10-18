2c9675413878ddbc15ae5cbd0f04877b3c220985
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-18 12:50:25 +0700

    Setup component and state

---
+++ src/Carousel.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import { shortList } from "./data";
+
+const Carousel = () => {
+  const [people, setPeople] = useState(shortList);
+
+  return <div>Carousel</div>;
+};
+
+export default Carousel;
