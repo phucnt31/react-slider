be19ea4b83d0f8760e8e97d7eaec7c452c7c6c62
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-18 13:39:03 +0700

    Render data and add Slider from react-slick

--- src/SlickCarousel.jsx
+++ src/SlickCarousel.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { list } from "./data";
+import Slider from "react-slick";
+import { FaQuoteRight } from "react-icons/fa";
 
 const SlickCarousel = () => {
-  return <div>SlickCarousel</div>;
+  return (
+    <section className="slider-container">
+      <Slider>
+        {list.map((person) => {
+          const { id, image, name, quote, title } = person;
+          return (
+            <article key={id} className="slide">
+              <img src={image} alt={name} className="person-img" />
+              <h5 className="name">{name}</h5>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon" />
+            </article>
+          );
+        })}
+      </Slider>
+    </section>
+  );
 };
 
 export default SlickCarousel;
